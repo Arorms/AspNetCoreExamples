9e9d4629bb9fa7c3685efd96ef951a5669892087
Holmes <holmesamzish86@outlook.com>
2025-07-09 21:23:15 +0800

    Update Movie MVC

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // View of HelloWorld index
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // GET: /HelloWorld/
+    //public string Index()
+    //{
+    //    return "This is my default action...";
+    //}
+
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int id = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name},  is: {id}");
+    }
+}
